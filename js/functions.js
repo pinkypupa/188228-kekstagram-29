e36d0c26dc4bc9fb2edb438473b51e7f7208d2d0
Leisan <lgabdrakhmanova@gmail.com>
2023-07-01 12:35:59 +0500

    Добавляет дополнительную задачу и доделывает старые

--- js/functions.js
+++ js/functions.js
@@ -1,33 +1,53 @@
 // Функция проверки количества символов строки
 
-let symbNum;
-
-function stringLength(stringCheck, maxSymb) {
-  symbNum = length(stringCheck);
-  if (symbNum <= maxSymb) {
-    return true;
+const checkString = function (someData, symbols) {
+  if (typeof someData === 'string') {
+    return (someData.length <= symbols);
   }
-  return false;
+  else console.log('Входные данные не являются строкой!');
 }
 
-stringLength();
+checkString('Лягушька', 4);
+
 
 // Функция проверки полиндрома
 
-let textPrepared;
-let textNew;
-let symbAdd;
-
 function reverseString(textToChange) {
-  textToChange.replaceAll(' ', '');
-  textToChange.toUpperCase();
-  textPrepared = textToChange;
-  textNew = '';
-  for (let i = (length(textPrepared) - 1); i > 0; i--) {
+  let symbAdd;
+
+  const textPrepared = textToChange.replaceAll(' ', '').toUpperCase();
+  let textNew = '';
+  for (let i = (textPrepared.length - 1); i >= 0; i--) {
     symbAdd = textPrepared[i];
-    textNew = textNew + symbAdd;
+    textNew += symbAdd;
   }
   return (textNew === textPrepared);
 }
 
-reverseString();
+reverseString('У О О оо у ');
+
+// Функция получения положительного числа
+
+const getNumber = function (anyString) {
+  let resultNumber = '';
+  if (typeof anyString !== 'string') {
+    anyString = anyString.toString();
+  }
+  for (let i = 0; i < anyString.length; i++) {
+    const newNumber = parseInt(anyString[i], 10);
+    if (!Number.isNaN(newNumber)) {
+      resultNumber += newNumber;
+    }
+  }
+  let finalResults = parseInt(resultNumber, 10);
+  finalResults = Number.isNaN(finalResults) ? console.log('NaN') : console.log('Итоговое число из строки: ' + finalResults);
+};
+
+getNumber('2023 год');
+getNumber('ECMAScript 2022');
+getNumber('1 кефир, 0.5 батона');
+getNumber('агент 007');
+getNumber('а я томат');
+getNumber(2023);
+getNumber(-1);
+getNumber(1.5);
